9d990900edda7352fe7569222a62c2736f665ff5
David Grant <mrdavidgrant@outlook.com>
2017-09-13 18:42:03 +0000

    logout working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 `use strict`
 const express = require('express')
-const morgan = require('morgan')
+let morgan = require('morgan')
 const bodyParser = require("body-parser");
 const urlDatabase = require('./urlDatabase')
 const methodOverride = require('method-override')
@@ -13,9 +13,13 @@ const PORT = process.env.PORT || 8080
 app.set('view engine', 'ejs')
 
 app.use(methodOverride('_method'))
-app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
+app.use((req, res, next) => { 
+  req.user = req.cookies.username
+  next() 
+}) 
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.redirect('/urls')
@@ -23,11 +27,11 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
   let data = urlDatabase.getAll()
-  res.render('urls_index', {url  :data})
+  res.render('urls_index', {url: data, user: req.user})
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", {user: req.user});
 });
 
 app.post("/urls", (req, res) => {
@@ -43,22 +47,32 @@ app.get("/u/:id", (req, res) => {
 app.get("/urls/:id/edit", (req, res) => {
   // console.log(req.params.id)
   let url = urlDatabase.getSingle(req.params.id)
-  res.render('urls_edit', {url: url})
+  res.render('urls_edit', {url: url, user: req.user})
 })
 
 app.patch('/urls/:id', (req, res) => {
   urlDatabase.editPair(req.params.id, req.body.longURL)
-  res.redirect('/urls')
+  res.redirect('/urls/:id')
 })
 
 app.get("/urls/:id", (req, res) => {
   // console.log(req.params.id)
   let url = urlDatabase.getSingle(req.params.id)
-  res.render('urls_show', {url: url})
+  res.render('urls_show', {url: url, user: req.user})
 })
 
 app.delete('/urls/:id/', (req, res) =>{
   urlDatabase.deletePair(req.params.id)
+  res.redirect('/urls')
+})
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
+})
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username', req.cookies.username)
   res.redirect('/urls')
 })
 
